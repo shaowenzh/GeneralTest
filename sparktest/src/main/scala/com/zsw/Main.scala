16382c80a347e9936b627de5d4588d074ad494ee
sz <zshaowen@126.com>
2019-08-21 15:50:55 +0800

    added spark kafka streaming

--- sparktest/src/main/scala/com/zsw/Main.scala
+++ sparktest/src/main/scala/com/zsw/Main.scala
@@ -1,8 +1,47 @@
 package com.zsw
 
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 
 object Main {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("test kafka project")
+    val streamingContext = new StreamingContext(conf, Seconds(20))
+
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> "localhost:9092",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "test_group",
+      "auto.offset.reset" -> "latest",
+      "enable.auto.commit" -> (false: java.lang.Boolean)
+    )
+
+    val topics = Array("my-replicated-topic")
+    val stream = KafkaUtils.createDirectStream[String, String](
+      streamingContext,
+      PreferConsistent,
+      Subscribe[String, String](topics, kafkaParams)
+    )
+
+    //stream.map(record => (record.key, record.value))
+    val line = stream.map(_.value())
+    val words = line.flatMap(_.split(" "))
+    val wordCounts = words.map((_, 1)).reduceByKey(_ + _)
+
+    wordCounts.print()
+
+    streamingContext.start()
+    streamingContext.awaitTermination()
+  }
+
+  /*
   def main(args: Array[String]) {
     // create Spark context with Spark configuration
     val sc = new SparkContext(new SparkConf().setAppName("Spark Count"))
@@ -24,4 +63,5 @@ object Main {
 
     System.out.println(charCounts.collect().mkString(", "))
   }
+  */
 }
