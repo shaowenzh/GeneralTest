6fedb70057f7c4c53392e0f8fc9b2331cf22cb19
sz <zshaowen@126.com>
2019-08-22 15:30:05 +0800

    added yarn client application

--- yarntest/src/test/java/YarnTest.java
+++ yarntest/src/test/java/YarnTest.java
@@ -1,7 +1,11 @@
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.yarn.api.protocolrecords.GetNewApplicationResponse;
 import org.apache.hadoop.yarn.client.api.YarnClient;
+import org.apache.hadoop.yarn.client.api.YarnClientApplication;
 import org.apache.hadoop.yarn.conf.YarnConfiguration;
+import org.apache.hadoop.yarn.server.MiniYARNCluster;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -36,6 +40,48 @@ public class YarnTest {
     }
 
     int a = 0;
+  }
+
+  @Test
+  public void miniYarnTest() {
+    Configuration conf = new YarnConfiguration();
+    int numNodeManagers = 1;
+    int numLocalDirs = 1;
+    int numLogDirs = 1;
+    boolean enableAHS;
+
+    /*
+     * Timeline service should not start if TIMELINE_SERVICE_ENABLED == false
+     * and enableAHS flag == false
+     */
+    conf.setBoolean(YarnConfiguration.TIMELINE_SERVICE_ENABLED, false);
+    enableAHS = false;
+    MiniYARNCluster cluster = null;
+    try {
+      cluster = new MiniYARNCluster(TestMiniYarnCluster.class.getSimpleName(),
+        numNodeManagers, numLocalDirs, numLogDirs, numLogDirs, enableAHS);
+      cluster.init(conf);
+      cluster.start();
+
+      YarnClient yarnClient = YarnClient.createYarnClient();
+      yarnClient.init(conf);
+      yarnClient.start();
+
+      YarnClientApplication app = yarnClient.createApplication();
+      GetNewApplicationResponse appResponse = app.getNewApplicationResponse();
+
+
+      //verify that the timeline service is not started.
+      Assert.assertNull("Timeline Service should not have been started",
+        cluster.getApplicationHistoryServer());
+    } catch (Exception e) {
+
+    }
+    finally {
+      if(cluster != null) {
+        cluster.stop();
+      }
+    }
   }
 
   private <T> ArrayList<T> getArrayList(Class<T> type) {
